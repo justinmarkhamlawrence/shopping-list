78b19a0e8cc59d7405ddd3af3825c4a57c5c3d4b
Justin Lawrence <justinmarkhamlawrence@gmail.com>
2016-06-13 15:10:30 -0400

    js

---
+++ js/do-stuff.js
@@ -0,0 +1,15 @@
+$('document').ready(function() {
+//form adds to "need it list"
+	$('#input').on("click", function() {
+		console.log("clicked")
+	});
+});	
+//button transfers item from "need it" to "got it"
+//button to delete items
+/*$('li').on('mouseenter', function() {
+	$('.kill', this).show();
+})
+$('li').on('mouseleave', function() {
+	$('.kill').hide();
+})*/
+/*console.log("I see you!") */
